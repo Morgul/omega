d3571c4730c375a57e1c54c18314be36d9849194
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-08 16:56:41 -0500

    Cleaned up middleware (moved most of it into the default settings.js file). Also added passport support, and exposed it on the app object as 'auth'.

--- scripts/file_templates/app/settings.js
+++ scripts/file_templates/app/settings.js
@@ -5,6 +5,7 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 var connect = require('connect');
+var passport = require('passport');
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -20,9 +21,18 @@ secret = "{{ secret }}";
 
 // Middleware
 middleware = [
-    // Put connect-compatible middleware here.
-    // connect.query(),
-    // some-other-middleware()
+    // Standard connect middleware
+    connect.query(),
+    connect.cookieParser(),
+    connect.session({
+        secret: secret,
+        key: 'sid',
+        cookie: { secure: true }
+    }),
+
+    // Authentication support
+    passport.initialize(),
+    passport.session()
 ];
 
 //----------------------------------------------------------------------------------------------------------------------
