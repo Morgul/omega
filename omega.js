f58d8a868e2cbcfe8ba7fb946b85b7cbb7b2eda4
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-10 16:19:08 -0500

    Initial addition of Sequelize.

--- omega.js
+++ omega.js
@@ -6,19 +6,24 @@
 
 var render = require('./util/render');
 var App = require('./lib/app').App;
-var Auth = require('./lib/auth').Auth;
+var AuthMan = require('./lib/auth').Auth;
+var ModelMan = require('./lib/models').ModelMan;
 
 // Create a new omega application
 var app = new App();
 
 // Create a new AuthManager
-var auth = new Auth();
+var auth = new AuthMan();
+
+// Create a new ModelManager
+var db = new ModelMan();
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 module.exports = {
     app: app,
     auth: auth,
+    db: db,
     utils: {
         render: render
     }
