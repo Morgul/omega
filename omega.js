e5002b7f6db8cd6f8bcd816c6eecc6ef0be95c9f
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-10 17:12:39 -0500

    Renamed, added ability to specify database in the config, and added missing packages.

--- omega.js
+++ omega.js
@@ -7,7 +7,6 @@
 var render = require('./util/render');
 var App = require('./lib/app').App;
 var AuthMan = require('./lib/auth').Auth;
-var ModelMan = require('./lib/models').ModelMan;
 
 // Create a new omega application
 var app = new App();
@@ -15,15 +14,15 @@ var app = new App();
 // Create a new AuthManager
 var auth = new AuthMan();
 
-// Create a new ModelManager
-var db = new ModelMan();
+// Create a database manager
+var DbMan = require('./lib/database').createDbMan(app.config);
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 module.exports = {
     app: app,
     auth: auth,
-    db: db,
+    db: DbMan,
     utils: {
         render: render
     }
