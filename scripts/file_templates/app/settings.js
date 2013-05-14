00e65a5b08f6765977f2be54f64d78f89b070941
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-14 15:49:09 -0500

    Settings template update.

--- scripts/file_templates/app/settings.js
+++ scripts/file_templates/app/settings.js
@@ -5,31 +5,43 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 var connect = require('connect');
-var passport = require('passport');
 
 //----------------------------------------------------------------------------------------------------------------------
 
 // Enables omega-wf debugging helpers. Disable this for production!
 DEBUG = true;
 
+// Omega will send email to the following addresses whenever an error occurs.
+admins = [
+    //["Your Name", "your.name@example.com"]
+];
+
 // Server settings
 listenAddress = "0.0.0.0";
 listenPort = 8080;
 
+// Uncomment this line to control the url that omega serves it's static files at.
+//omegaStaticUrl = '/omega';
+
 // Used for secure sessions. This should be unique per omega-wf application.
 secret = "{{ secret }}";
 
-// Middleware
+// Uncomment to setup/configure databases
+//databases = {
+//    default: {
+//        engine: 'sqlite',
+//        database: './{{ app }}.db'
+//    }
+//};
+
+// Uncomment to enable the admin section
+//omegaAdminUrl = '/admin';
+//useOmegaAdmin = true;
+
+// Connect Middleware
 middleware = [
     // Standard connect middleware
-    connect.query(),
-
-    // Not required, but recommended for auth
-    connect.cookieParser(secret),
-    connect.session({
-        secret: secret,
-        key: 'sid'
-    })
+    connect.query()
 ];
 
 //----------------------------------------------------------------------------------------------------------------------
