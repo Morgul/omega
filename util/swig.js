7fe20cf01bf55e596a2116d61253608b6ee72387
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-14 15:30:26 -0500

    Cleaned up all the internal rendering, and made it much nicer to use. Now, you can simply tell render that this is an internal call, and it will turn template paths into absolute paths for you. Also, we now inject DEBUG if the user hadn't overridden it.
    Also, added `omegaStaticUrl` to the config file; it defaults to `/omega`. This allows you to control the url at which omega serves it's static files. This is injected into templates as `static`, if the user hasn't overridden it.

--- util/swig.js
+++ util/swig.js
@@ -10,8 +10,33 @@ var swig = require('swig');
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-function render(response, template, context)
+function render(response, template, context, internal)
 {
+    var app = require('../omega').app;
+
+    // If we're rendering an internal template, we have to turn our template paths into absolute paths.
+    if(internal)
+    {
+        template = omegaPath(template);
+
+        if(!context['base'])
+        {
+            context['base'] = omegaPath("base.html");
+        } // end if
+    } // end if
+
+    // Check to see if the user has overridden 'static'. If not, add the path to the omega static files.
+    if(!context['static'])
+    {
+        context['static'] = app.omegaStaticUrl;
+    } // end if
+
+    // Check to see if the user has overridden 'DEBUG'. If not, add the app.config.DEBUG flag.
+    if(!context['DEBUG'])
+    {
+        context['DEBUG'] = app.config.DEBUG;
+    } // end if
+
     response.end(swig.compileFile(template).render(context));
 } // end render
 
